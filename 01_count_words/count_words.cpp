51d82ac2018566efdc47b86c17e3f1be92cf7025
Francois Chabot <francois.chabot.dev@gmail.com>
2018-03-04 19:45:02 -0500

    added helper function to exerc 01

--- 01_count_words/count_words.cpp
+++ 01_count_words/count_words.cpp
@@ -1,6 +1,14 @@
 #include "count_words.h"
 
 namespace exerc_01 {
+  namespace {
+    bool is_alpha(char c) {
+      return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+  }
+
+
+
   // N.B. The const& simply means:
   // - The function will only read from the variable without odifying it.
   // - The function won't need a copy of the variable.
